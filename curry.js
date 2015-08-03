6c0bb84112bc460d36f1d68ad74e18c785e08764
FilWisher <williamfisher@hotmail.co.uk>
2015-08-03 10:40:26 +0100

    add some linting and reformat

--- curry.js
+++ curry.js
@@ -1,11 +1,12 @@
 'use strict';
 
 module.exports = function curry (fn) {
-	return function () {
 
-        if (fn.length === arguments.length) {
-            return fn.apply(null, arguments);
-        }
-		return fn.bind.apply(fn, [null].concat([].slice.call(arguments)));
-	}
+  return function () {
+
+    if (fn.length === arguments.length) {
+      return fn.apply(null, arguments);
+    }
+    return fn.bind.apply(fn, [null].concat([].slice.call(arguments)));
+  };
 };
--- test.js
+++ test.js
@@ -2,41 +2,45 @@ var test = require('tape');
 var curry = require('./curry');
 
 function sum (a, b) {
-	return a + b
+  return a + b;
 }
 
 test('curry is a function', function (t) {
-	t.equal(typeof curry, 'function')
-	t.end()
-})
+
+  t.equal(typeof curry, 'function');
+  t.end();
+});
 
 test('curry returns a function when called on sum', function (t) {
-	t.equal(typeof curry(sum), 'function')
-	t.end()
-})
+
+  t.equal(typeof curry(sum), 'function');
+  t.end();
+});
 
 test('curry(sum)(2) returns a function', function (t) {
-	t.equal(typeof curry(sum)(2), 'function')
-	t.end()
-})
+
+  t.equal(typeof curry(sum)(2), 'function');
+  t.end();
+});
 
 test('curried sum called on 2 and then on 3 returns 5', function (t) {
-	t.equals(curry(sum)(2)(3), 5)
-	t.end()
-})
+
+  t.equals(curry(sum)(2)(3), 5);
+  t.end();
+});
 
 test('curried sum returns correct val when called in two stages on random nums', function (t) {
-	var x = Math.random();
-	var y = Math.random();
-	t.equals(curry(sum)(x)(y), x + y)
-	t.end()
-})
 
+  var x = Math.random();
+  var y = Math.random();
+  t.equals(curry(sum)(x)(y), x + y);
+  t.end();
+});
 
-//this should be new to this branch
 test('curried sum returns 5 when immediately given sufficient arguments', function (t) {
-	var x = Math.random();
-	var y = Math.random();
-	t.equals(curry(sum)(x, y), x + y)
-	t.end()
-})
+
+  var x = Math.random();
+  var y = Math.random();
+  t.equals(curry(sum)(x, y), x + y);
+  t.end();
+});
